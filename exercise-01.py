008a37072ff581105628698a9c44f5e51767c0d2
byfatim <heii.fatim@gmail.com>
2023-03-13 19:54:06 +0700

    Menambahkan file exercise-01

---
+++ exercise-01.py
@@ -0,0 +1,7 @@
+nama = 'Novita Arilfa'
+nim  = 'I0322095'
+
+print('Nama = ' + nama)
+print('NIM  = ' + nim)
+print('Saya lahir dan tinggal selama 17 tahun di kampung halaman saya yaitu Sukabumi. Sukabumi merupakan salah satu kota/kabupaten yang ada di Jawa Barat. Nama Sukabumi berasal dari bahasa Sunda, Suka-Bumen yang memiliki arti kawasan dengan udara sejuk dan nyaman untuk menetap. Sukabumi memiliki kekayaan alam yang sangat melimpah diantaranya yaitu laut, pegunungan, sawah, danau, dan perkebunan.')
+print('Sebagai lulusan Teknik Industri UNS saya memiliki harapan agar saya mendapatkan banyak ilmu yang berguna bagi masa depan. Teknik Industri juga menjadi salah satu mimpi saya yang harus saya wujudkan dan saya ingin menjadi panutan yang dapat menginspirasi banyak orang. Oleh karena itu, saya akan bersungguh-sungguh dalam menjalani perkuliahan di Teknik Industri UNS.')
---
+++ i love u.py
@@ -0,0 +1,6 @@
+nama_depan = 'novita'
+nama_belakang = 'arilfa'
+
+print(nama_depan)
+print('namaku novita')
+print('namaku' + nama_depan + 'lhoo')
